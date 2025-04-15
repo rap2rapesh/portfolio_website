218eb4841e305624348ba146a3512b14ebdf529e
rap2rapesh <rap2rapesh@gmail.com>
2025-04-15 17:59:11 +0530

    Update ProjectsSection.tsx
    added github code links

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -7,7 +7,7 @@ interface ProjectCardProps {
   description: string;
   image: string;
   tags: string[];
-  demoLink?: string;
+  // demoLink?: string;
   codeLink?: string;
 }
 const ProjectCard: React.FC<ProjectCardProps> = ({
@@ -15,7 +15,7 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
   description,
   image,
   tags,
-  demoLink,
+  // demoLink,
   codeLink
 }) => <div className="project-card rounded-xl overflow-hidden shadow-lg card-hover">
     <div className="h-56 overflow-hidden relative">
@@ -30,9 +30,6 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
             {tag}
           </Badge>)}
       </div>
-      
-      <div className="flex space-x-0 mx-[85px]">
-        {demoLink}
         
         {codeLink && <Button variant="outline" size="sm" className="flex items-center space-x-1 border-gray-300 text-gray-700 hover:bg-gray-100">
             <Github className="h-4 w-4" />
@@ -47,15 +44,13 @@ const ProjectsSection = () => {
     description: "Designed and implemented a real-time data streaming pipeline to monitor road journeys, contributing to enhanced road safety and accident prevention.",
     image: "https://private-user-images.githubusercontent.com/168437985/327924567-f6757c8a-2ab9-463e-81c8-60962c299a60.JPG?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NDQ3MjAxMDYsIm5iZiI6MTc0NDcxOTgwNiwicGF0aCI6Ii8xNjg0Mzc5ODUvMzI3OTI0NTY3LWY2NzU3YzhhLTJhYjktNDYzZS04MWM4LTYwOTYyYzI5OWE2MC5KUEc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjUwNDE1JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI1MDQxNVQxMjIzMjZaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT02MTIyOTUwMjBiZjM0ZWQ2NmQ5NTQ4NzEzZTk0M2I3YWQ5N2E2YTNhODQzMjZjYmNkYmZmMGNlYmM5OTcyMDc0JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.Sy0MbbSG3WrQt-1HqTVHUVUW0BUUH715pwm1RDmdQtI",
     tags: ["Kafka", "Zookeeper", "PySpark", "Docker", "AWS Redshift", "Glue"],
-    demoLink: "#",
-    codeLink: "#"
+    codeLink: "https://github.com/TylerDurden786/Rush_Hour"
   }, {
     title: "AI Agentic Medical Assistant",
     description: "Implemented a multi-modal AI solution combining speech-to-text, vision, language and text-to-speech capabilities which was built with Groq, OpenAI, Meta LLM and Gradio.",
     image: "https://github.com/TylerDurden786/ai_medical_assistant/raw/main/flowchart.JPG",
     tags: ["GROQ", "OpenAI", "Gradio UI", "Meta LLM", "Python"],
-    demoLink: "#",
-    codeLink: "#"
+    codeLink: "https://github.com/TylerDurden786/ai_medical_assistant"
   }];
   return <section id="projects" className="bg-gray-50 py-[5px]">
       <div className="container mx-auto px-4">
